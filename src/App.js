d8e3c94b3b66a9d30285ada1714eae50b289c8e8
bhakta <dashlucky248@gmail.com>
2018-10-23 21:10:23 +0530

    created new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hiii
+      </div>
+    );
+  }
+}
+
+export default App;
