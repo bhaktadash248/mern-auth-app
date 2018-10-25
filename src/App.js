527f27479cdae1010cd682bddfac9e1734e7ee6f
bhakta <dashlucky248@gmail.com>
2018-10-26 00:16:28 +0530

    updated project filder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hiii
+      <BrowserRouter>
+      <div>
+          hiii
       </div>
+      </BrowserRouter>
     );
   }
 }
